4fb420b90ba718a4384e80a444ea520c14219d2c
Amit <amitsingh5412@gmail.com>
2023-11-20 10:24:46 -0800

    added

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
+HEAD
 Hello I am updating this line from main branch
-=======
-Updating this line from dev branch
->>>>>>> development
+Updating this line from dev branch development
 
 this files contains the deployment script
 
 
-updating in dev branch
 
 
